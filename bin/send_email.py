b77c4c4a822749542024dc1fa77f60c82da48a57
Jenny Medina <medina.stsci@gmail.com>
2024-02-01 19:06:04 -0500

    Adding type hints, args, and returns to get_participant_id

--- bin/send_email.py
+++ bin/send_email.py
@@ -3,12 +3,21 @@
 import sys
 import synapseclient
 
+from typing import List
 
-def get_participant_id(syn, submission_id):
+
+def get_participant_id(syn: synapseclient.Synapse, submission_id: str) -> List[str]:
     """
     Retrieves the teamId of the participating team that made
     the submission. If the submitter is an individual rather than
     a team, the userId for the individual is retrieved.
+
+    Arguments:
+      syn: A Synapse Python client instance
+      submission_id : The ID for an individual submission within an evaluation queue
+
+    Returns:
+      Returns the synID of a team or individual participant
     """
     # Retrieve a Submission object
     submission = syn.getSubmission(submission_id, downloadFile=False)
