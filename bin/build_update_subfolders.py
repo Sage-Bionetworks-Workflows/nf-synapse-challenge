5076d38c56241076ca6803025e86d03a9366380e
Jenny Medina <medina.stsci@gmail.com>
2024-02-06 18:27:59 -0500

    Updating calls to parent_folder_id, project_id

--- bin/build_update_subfolders.py
+++ bin/build_update_subfolders.py
@@ -112,20 +112,20 @@ def build_update_subfolders(
     # Establish access to the Synapse API
     syn = synapseclient.login()
 
-    parent_folder_id, project_id = get_parent_and_project_folder_id(syn, parent_folder, project_name)
+    synapse_ids = get_parent_and_project_folder_id(syn, parent_folder, project_name)
     submitter_id = send_email.get_participant_id(syn, submission_id)[0]
 
     if build_or_update == "build":
 
         # Creating the level 1 (directly under Parent-Folder/) subfolder, which is named
         # after the submitters' team/userIds.
-        level1_subfolder = build_subfolder(syn, folder_name=submitter_id, parent_folder=parent_folder_id)
+        level1_subfolder = build_subfolder(syn, folder_name=submitter_id, parent_folder=synapse_ids.parent_folder_id)
         # Creating the level 2 subfolders that live directly under submitter subfolder.
         for level2_subfolder in subfolders:
             level2_subfolder = build_subfolder(syn, folder_name=level2_subfolder, parent_folder=level1_subfolder)
             # Update the permissions for the folder that should only be accessed by Challenge admins.
             if level2_subfolder.name == only_admins:
-                update_permissions(syn, level2_subfolder, project_id)
+                update_permissions(syn, level2_subfolder, synapse_ids.project_id)
 
 
     # TODO: https://sagebionetworks.jira.com/browse/IBCDPE-809
