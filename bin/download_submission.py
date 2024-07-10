9c0d208dc18c3c061fca8a09f9c41626f929be20
bwmac <bwm03570@gmail.com>
2024-07-10 12:23:44 -0600

    adds download location

--- bin/download_submission.py
+++ bin/download_submission.py
@@ -9,7 +9,7 @@ submission_id = sys.argv[1]
 syn = synapseclient.Synapse()
 syn.login(silent=True)
 
-submission = syn.getSubmission(submission_id)
+submission = syn.getSubmission(submission_id, downloadLocation=".")
 entity_type = submission["entity"].concreteType
 if entity_type != "org.sagebionetworks.repo.model.FileEntity":
     open("dummy.txt", 'w').close()
